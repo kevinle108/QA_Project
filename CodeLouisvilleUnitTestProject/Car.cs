cb9379f818b0b1afeddb212ce396e610e8e409b1
Kevin Le <kevinle108@gmail.com>
2022-11-12 00:39:03 -0500

    some car class and tests

---
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLouisvilleUnitTestProject
+{
+    public class Car : Vehicle
+    {
+        private HttpClient _client;
+        private string _baseAddress = "https://vpic.nhtsa.dot.gov/api/";
+        public int NumberOfPassengers { get; set; }
+
+        public Car() 
+            : this(0, "", "", 0)
+        {
+
+        }
+
+        public Car(double gasTankCapacity, string make, string model, double milesPerGallon)
+            : base(4, gasTankCapacity, make, model, milesPerGallon)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(_baseAddress);
+        }
+
+    }
+}
---
+++ CodeLouisvilleUnitTestProjectTests/CarTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeLouisvilleUnitTestProject;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Diagnostics;
+
+namespace CodeLouisvilleUnitTestProjectTests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void CreatedCarIsValidWith4Tires()
+        {
+            var car = new Car();
+            using (new AssertionScope())
+            {
+                car.Should().NotBeNull();
+                car.NumberOfTires.Should().Be(4);
+            }
+        }
+    }
+}
